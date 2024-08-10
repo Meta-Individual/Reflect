34cd8adaf9f9a2d497028b57a82b20f2636f2a3a
meo-young <meoyoung000@naver.com>
2024-08-10 23:37:50 +0900

    2층전까지 대사구현

--- Assets/Script/Interact/Mansion_Inside/KannaAnimation.cs
+++ Assets/Script/Interact/Mansion_Inside/KannaAnimation.cs
@@ -5,15 +5,20 @@ using UnityEngine;
 public class KannaAnimation : MonoBehaviour, IInteractable
 {
     public string objectID;
-    private bool isSearched = false;
+    public string itemName = "KannaInteract";
     public Sprite image;
+
+    private bool isSearched = false;
     private IllustManager _illustManager;
     private PlayerController _playerController;
+    private PlayerInventory playerInventory;
+
 
     void Start()
     {
         _illustManager = FindObjectOfType<IllustManager>();
         _playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        playerInventory = FindObjectOfType<PlayerInventory>();
     }
     public void Interact()
     {
@@ -22,6 +27,7 @@ public class KannaAnimation : MonoBehaviour, IInteractable
             isSearched = true;
             _illustManager.ShowIllust(image, objectID);
             _playerController.kannaAnim = true;
+            playerInventory.AddItem(itemName);
         }
         else
         {
