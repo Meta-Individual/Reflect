512a29b6b4db8971afe854f9b172aa7e8543e507
junghyungo <pp90971658@gmail.com>
2024-08-20 18:27:30 +0900

    오프닝 해상도 및 사운드 싱크

--- Assets/Script/Interact/OpeningManager.cs
+++ Assets/Script/Interact/OpeningManager.cs
@@ -7,6 +7,8 @@ public class OpeningManager : MonoBehaviour
 {
     public TMP_Text text;
     private string dialogue;
+    public AudioSource _audioSource;
+    public AudioClip typingSound;
     public static OpeningManager Instance { get; private set; } // Singleton 인스턴스
 
     private void Start()
@@ -16,7 +18,7 @@ public class OpeningManager : MonoBehaviour
 
     public void ShowOpening()
     {
-        dialogue = "... 그림자집?Q그 다이세츠 지역의..?Q네, 그곳에 관한 이야기입니다.";
+        dialogue = ". . 그림자집?Q. . . . . 그 다이세츠 지역의?Q네, 그곳에 관한 이야기입니다.";
         StartCoroutine(ActiveOpening(dialogue));
     }
     
@@ -24,7 +26,9 @@ public class OpeningManager : MonoBehaviour
     {
         // yield return new WaitForSeconds(2.0f);
         text.text = null;
-
+        _audioSource.clip = typingSound;
+        _audioSource.loop = true;
+        _audioSource.Play();
         for (int i =0 ; i < dialogue.Length ; i++)
         {
             if(dialogue[i] == 'Q')
@@ -38,6 +42,7 @@ public class OpeningManager : MonoBehaviour
                 yield return new WaitForSeconds(0.1f);     
             }        
         }
+        _audioSource.Stop();
     }
 }
 
