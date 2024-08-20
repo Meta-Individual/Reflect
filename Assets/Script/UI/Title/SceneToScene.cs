b1cbf4853165f9012df263bea794bddd59454f8a
junghyungo <pp90971658@gmail.com>
2024-08-20 17:48:55 +0900

    please

---
+++ Assets/Script/UI/Title/SceneToScene.cs
@@ -0,0 +1,22 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.SceneManagement;
+// public class SceneToScene : MonoBehaviour 
+// {
+//     public gameObject ReadyToStart;
+//     public string nextSceneName = "MansionScene";
+
+//     public void Start()
+//     {
+//         ReadyToStart = ReadyToStart.GetComponent<gameObject>();
+//     }
+//     public void Update()
+//     {
+//         if (ReadyToStart.isActiveAndEnabled)
+//         {
+//             SceneManager.LoadScene(nextSceneName);
+//         }
+       
+//     }
+// }
